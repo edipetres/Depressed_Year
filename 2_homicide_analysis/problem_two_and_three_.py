ba2dfbd62cfb748e0922b7f810a759fed073d4de
Nexao <emilklausen2@hotmail.com>
2017-03-14 21:11:54 +0100

    Add files via upload
    Problem two and three added

---
+++ 2_homicide_analysis/problem_two_and_three_.py
@@ -0,0 +1,8 @@
+import urllib.request
+import os
+
+def download(url, path = './'):
+    filename = url.split("/")[-1]
+    fullfilename = os.path.join(path, filename)
+    file_name, headers = urllib.request.urlretrieve(url, fullfilename)
+    print("File saved to " + path + " as " + filename)
