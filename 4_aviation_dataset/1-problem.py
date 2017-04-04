88d61deb8779e0c6d457fe0c414cda9c20406dc4
Lucas Meulengracht Fredmark <lucas.m.fredmark@gmail.com>
2017-04-04 20:43:47 +0000

    Added solution for question 2 and plots

--- 4_aviation_dataset/1-problem.py
+++
@@ -1,8 +0,0 @@
-#How do the flight phases (ex. take off, cruise, landing..) contribute to fatalities? Chart!
-
-import csv
-import webget
-import requests
-
-url = 'https://raw.githubusercontent.com/edipetres/Depressed_Year/master/Dataset_Assignment/AviationDataset.csv'
-requests.get(url, params={'downloadformat' : 'csv'})
